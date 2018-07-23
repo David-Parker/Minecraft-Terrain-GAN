feabd11bbeb7a7dc5c0c050b577c1c4255a57d50
Allen Wang <t-alwa@microsoft.com>
2018-07-23 23:18:53 +0000

    Added in a terrain data loader

---
+++ Machine-Learning/dataloader.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Data Loader for terrain generator
+"""
+import keras 
+import threading
+import numpy as np
+import os
+
+from keras import backend as K
+
+def load_terrain(file_path):
+    return np.loadtxt(file_path, delimiter=',').reshape(16,16,16)
+
+class TerrainDataLoader(keras.utils.Sequence):
+    def __init__(self, directory,
+                 batch_size=32, shuffle=True,
+                 terrain_shape=(16, 16, 16),
+                 seed=None):
+        self.directory = directory
+        self.batch_size = batch_size
+        self._load_data()
+        self.index_array = None
+        self.shuffle = shuffle
+        self.seed = seed
+        self.total_batches_seen = 0
+        self.lock = threading.Lock()
+        self.index_generator = self._flow_index()
+        self.terrain_shape = terrain_shape
+
+        print ("Found %d terrain files." % self.n)
+
+        super(TerrainDataLoader, self).__init__()
+
+    def _load_data(self):
+        file_paths = []
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for filename in filenames:
+                if filename.endswith('.txt'):
+                    file_paths.append(os.path.join(dirpath, filename))
+        self.file_paths = file_paths
+        self.n = len(file_paths)
+
+    def __len__(self):
+        return (self.n + self.batch_size - 1) // self.batch_size
+
+    def on_epoch_end(self):
+        self._set_index_array()
+
+    def reset(self):
+        self.batch_index = 0
+
+    def _flow_index(self):
+        self.reset()
+        while 1:
+            if self.seed is not None:
+                np.random.seed(self.seed + self.total_batches_seen)
+            if self.batch_index == 0:
+                self._set_index_array()
+
+            current_index = (self.batch_index * self.batch_size) % self.n
+            if self.n > current_index + self.batch_size:
+                self.batch_index += 1
+            else:
+                self.batch_index = 0
+
+            self.total_batches_seen += 1
+            yield self.index_array[current_index:
+                                   current_index + self.batch_size]
+
+    def _set_index_array(self):
+        self.index_array = np.arange(self.n)
+        if self.shuffle:
+            self.index_array = np.random.permutation(self.n)
+
+    def _get_batch_of_samples(self, index_array):
+        batch_x = np.zeros(
+                (len(index_array),) + self.terrain_shape,
+                 dtype=K.floatx())
+        for i, j in enumerate(index_array):
+            fname = self.file_paths[j]
+            terrain = load_terrain(os.path.join(self.directory, fname))
+            batch_x[i] = terrain
+        return batch_x
+
+    def __getitem__(self, idx):
+        if idx >= len(self):
+            raise ValueError('Asked to retrieve element {idx}, '
+                             'but the Sequence '
+                             'has length {length}'.format(idx=idx,
+                                                          length=len(self)))
+        if self.seed is not None:
+            np.random.seed(self.seed + self.total_batches_seen)
+        self.total_batches_seen += 1
+        if self.index_array is None:
+            self._set_index_array()
+        index_array = self.index_array[self.batch_size * idx:
+                                       self.batch_size * (idx + 1)]
+        batch = self._get_batch_of_samples(index_array)
+        return batch
+
+    def __iter__(self):
+        return self
+
+    def __next__(self, *args, **kwargs):
+        with self.lock:
+            index_array = next(self.index_generator)
+        return self._get_batch_of_samples(index_array)
