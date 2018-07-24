15fa3b301d07b9925cabc4025e724a12d2f1858f
David Parker <david@DESKTOP-GLQJM18.redmond.corp.microsoft.com>
2018-07-24 10:56:29 -0700

    Hooked up world generator to file reading

--- Unity/Assets/Scripts/LidarDataTest.cs
+++ Unity/Assets/Scripts/LidarDataTest.cs
@@ -8,14 +8,14 @@ namespace Assets.Scripts
 {
     public class LidarDataTest
     {
-        public static LidarWorldData[] Load16CubeDataSet(string filepath, int cubeCount)
+        public static LidarWorldData[] Load16CubeDataSet(string filepath, int cubeCount, Vector3Int worldDimensions)
         {
             string[] cubeLines = System.IO.File.ReadAllLines(filepath);
             var result = new LidarWorldData[cubeCount];
             for (int cubeIndex = 0; cubeIndex < cubeCount; cubeIndex++)
             {
                 bool[] dataPoints = cubeLines[cubeIndex].Split(new[]{','}).Select(x => x.Equals("1") ? true : false).ToArray();
-                result[cubeIndex] = new LidarWorldData(dataPoints, LidarConstants.ChunkSize, LidarConstants.ChunkSize, LidarConstants.ChunkSize);
+                result[cubeIndex] = new LidarWorldData(dataPoints, worldDimensions.X, worldDimensions.Y, worldDimensions.Z);
             }
             // string[] cubeLines = System.IO.File.ReadAllLines(@"C:\Src\Hackathon2018\Minecraft-Terrain-GAN\Data\dummy.txt");
             return result;
